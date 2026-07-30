3a18a35ebe87bf3f5cf70a127500bc791a93599b
Pavel <yatrushevpm@gmail.com>
2026-07-30 13:43:59 +0300

    Initial commit for mass feature, added conversion table

--- internal/converter/converter.go
+++ internal/converter/converter.go
@@ -3,11 +3,11 @@ package converter
 import "github.com/PavYtr/nofreedom/internal/units"
 
 func Convert(value float64, from string, to string) (float64, error) {
-	fromUnit, ok := units.RegistryLength[from]
+	fromUnit, ok := units.Registry[from]
 	if !ok {
 		return 0, ErrUnknownUnit{Unit: from}
 	}
-	toUnit, ok := units.RegistryLength[to]
+	toUnit, ok := units.Registry[to]
 	if !ok {
 		return 0, ErrUnknownUnit{Unit: to}
 	}
